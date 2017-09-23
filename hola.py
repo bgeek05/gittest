3f2c1c4f7f521f9516ebc116cb55c445faf5fd23
bgeek05 <bryanarenas58@gmail.com>
2017-09-23 11:20:36 -0500

    Actualizar hola.py

--- hola.py
+++ hola.py
@@ -1,3 +1,3 @@
 nombre = input('cuál es tu nombre?: ')
 apellido = input('cuál es tu apellido?: ')
-print('hola {} {}'.format(nombre, apellido))
+print('hola {} {} qué tal?'.format(nombre, apellido))
